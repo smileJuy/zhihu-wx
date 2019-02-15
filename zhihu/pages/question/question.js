e4ab920dd3985c7487375a2b15ae4eac8136b9b8
smileJuy <615935011@qq.com>
2019-02-15 15:08:01 +0800

    fix

--- zhihu/pages/question/question.js
+++ zhihu/pages/question/question.js
@@ -1,66 +1,36 @@
 // pages/question/question.js
+var util = require('../../utils/util.js');
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    motto:'知乎-微信小程序版',
+    userInfo:{}
   },
-
+  // 事件处理函数
+  bindItemTap:function(){
+    wx.navigateTo({
+      url:'../answer/answer'
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log('onLoad');
+    var that = this;
+    // 调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      that.setData({
+        userInfo:userInfo
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  tapName: function (event){
+    console.log(event);
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+ 
 })
